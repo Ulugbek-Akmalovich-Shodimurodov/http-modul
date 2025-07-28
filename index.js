a1e1dfccb8d94b6fe0591d7c44ef96b645ff9850
Ulugbek-Akmalovich-Shodimurodov <99031894+Ulugbek-Akmalovich-Shodimurodov@users.noreply.github.com>
2025-07-28 12:22:52 +0500

    apis

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const http = require('http');
-const server = http.createServer();
-server.on('connection', (socket)=>{
-    console.log('Yangi bog\'lanish...');
+const server = http.createServer((req, res)=>{
+    if(req.url === '/'){
+        res.write('Asosiy sahifa');
+        res.end();
+    }
+    if(req.url === '/api/books'){
+        res.write(JSON.stringify(['1 book', '2 book', '3 book']));
+        res.end();
+    }
+    // console.log('Yangi bog\'lanish...');
 })
 server.listen(8000);
 console.log(`${server.address().port} portni eshitishni boshladim...`);
